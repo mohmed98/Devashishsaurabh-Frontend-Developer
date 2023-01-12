3e173675920d3d89ae632d7c818386b22cbdcd8a
Devashishsaurabh <onlyvishwasindia@gmail.com>
2023-01-12 13:50:11 +0530

    Navbar & Banner done

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div style={{height:"600px"}}>
+        <img class="h-full w-full"src="https://www.teslarati.com/wp-content/uploads/2018/08/Crew-Dragon-DM-1-orbit-tall-pano-SpaceX-2.jpg" alt="Banner"/>
+    </div>
+  )
+}
+
+export default Banner
